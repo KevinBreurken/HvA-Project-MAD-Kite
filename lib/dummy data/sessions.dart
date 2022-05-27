0f2c05c0c4396d329c6f6a049b071031a21ed011
Quilliano van Hal <quilliano.van.hal@hva.nl>
2022-05-27 15:54:43 +0200

    Start for sessions page
    Function search bar, list view with listtiles.
    
    TO DO: custom listtiles to display the rating the user gave the session

---
+++ lib/dummy data/sessions.dart
@@ -0,0 +1,23 @@
+class Session{
+  final String locationName;
+  final String gear;
+  final num rating;
+  final String date;
+  final String start;
+  final String end;
+  
+  const Session({
+    required this.locationName,
+    required this.gear,
+    required this.rating,
+    required this.start,
+    required this.end,
+    required this.date
+  });
+}
+
+const allSessions = [
+  Session(locationName: 'Castricum aan zee', gear: '9m2, twin-tip', rating: 4, start: '14:00', end: '15:33', date: '2022-3-12'),
+  Session(locationName: 'Zeeland', gear: '9m2, twin-tip', rating: 5, start: '14:00', end: '15:33', date: '2022-5-30'),
+  Session(locationName: 'Limburg', gear: '9m2, twin-tip', rating: 1, start: '14:00', end: '15:33', date: '2021-5-21'),
+];
--- lib/page/my_sessions_page.dart
+++ lib/page/my_sessions_page.dart
@@ -1,8 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:kiteup/dummy%20data/sessions.dart';
+import 'package:kiteup/helpers/hexcolor.dart';
+import 'package:kiteup/constants.dart';
 
-class MySessionsPage extends StatelessWidget {
+class MySessionsPage extends StatefulWidget {
+    @override
+  _MySessionsPageState createState() => _MySessionsPageState();
+}
+
+class _MySessionsPageState extends State<MySessionsPage>
+  with SingleTickerProviderStateMixin {
+  List<Session> sessions = allSessions;
+  final controller = TextEditingController();
+  
   @override
   Widget build(BuildContext context) => Scaffold(
       appBar: null,
-      body: Center(child: Text('List', style: TextStyle(fontSize: 60))));
+      backgroundColor: DARK_BACKGROUND_PRIMARY,
+      body: Column(
+        children: <Widget> [
+          Container(
+            margin: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+            child: TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: HexColor('848484'),
+                prefixIcon: const Icon(Icons.search),
+                hintText: 'Search...',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: PRIMARY),
+                  borderRadius: BorderRadius.circular(30)
+                )
+              ),
+              onChanged: searchSession,
+            ),
+
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: sessions.length, 
+              itemBuilder: (context, index) {
+                final session = sessions[index];
+
+                return Container(
+                  margin: const EdgeInsets.fromLTRB(16, 4, 16, 4),
+                  child: ListTile(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                    title: Text(session.locationName),
+                    subtitle: Text(session.gear),
+                    isThreeLine: true,
+                    trailing: Text(session.date + "\n" + session.start + " - " + session.end, textAlign: TextAlign.right,),
+                    tileColor: HexColor('848484'),
+                  ),
+                );
+            }
+          ),
+          )
+        ]
+
+      )
+
+      );
+      
+
+  void searchSession(String query) {
+    final suggestions = allSessions.where((session){
+      final sessionLocation = session.locationName.toLowerCase();
+      final input = query.toLowerCase();
+
+      return sessionLocation.contains(input);
+    }).toList();
+
+    setState(() => sessions = suggestions);
+  }
 }
