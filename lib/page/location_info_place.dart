618852c9e9be803b7f66b0892a0b8c5b70191c91
R.Nithoer <shano.nithoer@hva.nl>
2022-06-13 15:22:00 +0200

    Location info page activity log werkt nu ook.
    Ik heb 2 extra classes gemaakt voor de dummy data en de koppeling tussen locatie en gebruiker door middel van "Event".

--- lib/page/location_info_place.dart
+++ lib/page/location_info_place.dart
@@ -1,6 +1,12 @@
+// ignore_for_file: sized_box_for_whitespace
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:kiteup/constants.dart';
+import 'package:kiteup/dummy%20data/locations.dart';
+import 'package:kiteup/dummy%20data/users.dart';
 import 'package:kiteup/notifiers/notifier_kiteup_status.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -8,6 +14,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../notifiers/notifier_selected_location.dart';
 import '../widgets/modals/modal_session.dart';
 import '../widgets/modals/modal_session_data.dart';
+
+import 'package:kiteup/dummy%20data/events.dart';
 
 class KiteupLocationPage extends StatefulWidget {
   KiteupLocationPage();
@@ -19,6 +27,7 @@ class KiteupLocationPage extends StatefulWidget {
 class _LocationInfoPageState extends State<KiteupLocationPage> {
   TimeOfDay selectedTime = TimeOfDay.now();
   String goingText = " Going";
+  List<Event> events = [];
 
   _LocationInfoPageState();
 
@@ -28,13 +37,30 @@ class _LocationInfoPageState extends State<KiteupLocationPage> {
     });
   }
 
+  addEventItem(Event event){
+    setState(() {
+      allEvents.add(event);
+    });
+  }
+
+  addEvents(List<Event> events){
+    setState(() {
+      this.events.clear();
+      this.events.addAll(events);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final _selectedLocationNotifier =
         Provider.of<SelectedLocationNotifier>(context);
+        
+    final _kiteupStatusNotifier =
+        Provider.of<KiteupStatusNotifier>(context);
 
-    final _kiteupStatusNotifier = Provider.of<KiteupStatusNotifier>(context);
-
+    List<Event> matchingEvents = fetchEvents(_selectedLocationNotifier.selectedLocation);
+    addEvents(matchingEvents);
+    
     return Scaffold(
         appBar: null,
         body: Scaffold(
@@ -76,73 +102,34 @@ class _LocationInfoPageState extends State<KiteupLocationPage> {
                           fontSize: 20,
                         )))),
             Expanded(
-                child: SizedBox(
-                    height: 200,
-                    child: ListView(
-                      padding:
-                          const EdgeInsetsDirectional.fromSTEB(15, 5, 0, 0),
-                      children: <Widget>[
-                        Container(
-                          height: 25,
-                          child: Center(
-                              child: Row(
-                            children: const [
-                              Text("10:22 ",
-                                  style: TextStyle(color: Colors.white)),
-                              SizedBox(width: 20),
-                              Text("Ryan Stanford will arrive at 14:00",
-                                  style: TextStyle(color: Colors.white)),
-                            ],
-                          )),
-                        ),
-                        Container(
-                          height: 25,
-                          child: Center(
-                              child: Row(
-                            children: const [
-                              Text("11:03 ",
-                                  style: TextStyle(color: Colors.white)),
-                              SizedBox(width: 20),
-                              Text("Cassidy Muels will arrive at 14:00",
-                                  style: TextStyle(color: Colors.white)),
-                            ],
-                          )),
-                        ),
-                        Container(
-                          height: 25,
-                          child: Center(
-                              child: Row(
-                            children: const [
-                              Text("12:17 ",
-                                  style: TextStyle(color: Colors.white)),
-                              SizedBox(width: 20),
-                              Text("Verny Seeker will arrive at 13:00",
-                                  style: TextStyle(color: Colors.white)),
-                            ],
-                          )),
-                        ),
-                        Container(
-                          height: 25,
-                          child: Center(
-                              child: Row(
-                            children: const [
-                              Text("12:52 ",
-                                  style: TextStyle(color: Colors.white)),
-                              SizedBox(width: 20),
-                              Text("Alsof Maker will arrive at 13:15",
-                                  style: TextStyle(color: Colors.white)),
-                            ],
-                          )),
-                        ),
-                      ],
-                    ))),
+              child: SizedBox(
+                height: 200,
+                child: ListView.builder(
+                  itemBuilder: (context, position) {
+                    Event event = events[position];
+                    return Container(
+                      height: 25,
+                      child: Center(child: Row(
+                        children: [
+                          Text(event.createdAt.hour.toString() + ":" + event.createdAt.minute.toString(), style: const TextStyle(color: Colors.white)),
+                          const SizedBox(width: 20),
+                          Text(event.user.name + " will arive at " + (event.datetime!.hour.toString() + ":" + event.datetime!.minute.toString()), style: const TextStyle(color: Colors.white)),
+                        ],
+                      )),
+                    );
+                  },
+                  padding: const EdgeInsetsDirectional.fromSTEB(15, 5, 0, 0),
+                  itemCount: events.length,
+                )
+              )
+            ),
             ButtonBar(
               mainAxisSize: MainAxisSize.min,
               buttonHeight: 10,
               children: [
                 GestureDetector(
                   onTap: () {
-                    _selectTime(context, _kiteupStatusNotifier);
+                    _selectTime(context, _kiteupStatusNotifier, _selectedLocationNotifier);
                   },
                   child: Container(
                     width: 120,
@@ -162,7 +149,7 @@ class _LocationInfoPageState extends State<KiteupLocationPage> {
                           width: 20,
                           color: Colors.white,
                         ),
-                        Text('$goingText',
+                        Text(goingText,
                             style: const TextStyle(color: Colors.white)),
                       ],
                     ),
@@ -205,9 +192,9 @@ class _LocationInfoPageState extends State<KiteupLocationPage> {
                     ),
                     padding: const EdgeInsets.all(10),
                     child: Row(
-                      children: [
+                      children: const [
                         Icon(Icons.check, color: Colors.white),
-                        const Text("Kite-up",
+                        Text("Kite-up",
                             style: TextStyle(color: Colors.white)),
                       ],
                     ),
@@ -219,19 +206,39 @@ class _LocationInfoPageState extends State<KiteupLocationPage> {
         ));
   }
 
-  _selectTime(BuildContext context, _kiteupStatusNotifier) async {
-    final TimeOfDay? timeOfDay = await showTimePicker(
-      context: context,
-      initialTime: selectedTime,
-      initialEntryMode: TimePickerEntryMode.dial,
-    );
+  fetchEvents(Location location){
+    return allEvents.where((event) => event.location == location && event.datetime != null && event.type == EventType.GOING).toList();
+  }
+  
+  _selectTime(BuildContext context, _kiteupStatusNotifier, _selectedLocationNotifier) async {          
+      final TimeOfDay? timeOfDay = await showTimePicker(
+        context: context,
+        initialTime: selectedTime,
+        initialEntryMode: TimePickerEntryMode.dial,
+      );
 
-    if (timeOfDay != null && timeOfDay != selectedTime) {
-      setGoingText(" " + timeOfDay.format(context));
-      _kiteupStatusNotifier.updateStatus("Traveling");
-    } else {
-      setGoingText(" Going");
-      _kiteupStatusNotifier.updateStatus("");
-    }
+      if(timeOfDay != null && timeOfDay != selectedTime)
+        {
+          final now = DateTime.now();
+          final newDateTime = DateTime(now.year, now.month, now.day, timeOfDay.hour, timeOfDay.minute);
+          final diff = newDateTime.difference(now);
+
+          // Add new event to the list of events
+          Event event = Event(
+            location: _selectedLocationNotifier.selectedLocation,
+            user: allUsers[0],
+            createdAt: DateTime.now(),
+            type: EventType.GOING,
+            datetime: DateTime.now().add(diff),
+          );
+
+          setGoingText(" " + timeOfDay.format(context));
+          _kiteupStatusNotifier.updateStatus("Traveling");
+          
+          addEventItem(event);
+        } else {
+          setGoingText(" Going");
+          _kiteupStatusNotifier.updateStatus("");
+        }
   }
 }
